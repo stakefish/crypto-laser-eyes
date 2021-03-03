7168bbf2d212bc3f8c2f5b85edb4e30c93ff9a0f
apple-juice <daycoder@icloud.com>
2021-03-03 17:50:44 +0300

    improve laser cards layout

--- src/components/Variant.tsx
+++ src/components/Variant.tsx
@@ -11,36 +11,35 @@ export interface VariantProps {
 }
 
 const Laser = styled.div<VariantProps>`
-  background: ${(props) => props.theme.colors.black} url(${(props) => props.image}) center / cover;
+  background-color: ${(props) => props.theme.colors.black};
   padding: ${rem(20)};
   border-radius: ${rem(32)};
-  width: ${rem(138)};
-  height: ${rem(138)};
+  width: 100%;
+  height: 0;
+  padding-bottom: 85%;
   position: relative;
-  transition: all ${(props) => props.theme.transition.base};
+  transition: box-shadow ${(props) => props.theme.transition.base};
   color: ${(props) => props.theme.colors.gray};
   cursor: pointer;
+  overflow: hidden;
 
-  div {
-    padding: ${rem(14)} ${rem(14)} 0;
+  &:before {
+    content: "";
     position: absolute;
-    top: 100%;
     left: 0;
+    top: 0;
     width: 100%;
-    text-align: center;
-    word-break: break-word;
-  }
-
-  span {
-    border-radius: ${rem(8)};
-    display: inline-flex;
-    padding: ${rem(2)} ${rem(4)};
-    transition: background-color ${(props) => props.theme.transition.base};
+    height: 100%;
+    background: url(${(props) => props.image}) center / cover;
+    transition: transform 2s ease;
   }
 
   &:hover {
     box-shadow: inset 0 0 0 5px rgba(119, 123, 132, 0.24), 0 0 0 2px #777b84;
-    color: ${(props) => props.theme.colors.white};
+
+    &:before {
+      transform: scale(1.15) rotate(10deg);
+    }
   }
 
   ${(props) =>
@@ -48,10 +47,6 @@ const Laser = styled.div<VariantProps>`
     css`
       &:before {
         box-shadow: inset 0 0 0 5px rgba(252, 114, 87, 0.24), 0 0 0 2px #fc7459;
-      }
-
-      span {
-        background-color: #fb7358;
       }
     `}
 `
--- src/containers/Lasers.tsx
+++ src/containers/Lasers.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Grid, Row, Col } from "react-styled-flexboxgrid"
+import styled from "styled-components"
 
 import { Laser } from "../helpers/types"
 import { LASERS } from "../helpers/const"
@@ -16,6 +17,16 @@ interface Props {
   onClick: (laser: Laser) => void
 }
 
+const StyledCol = styled.div`
+  flex: 0 0 25%;
+  margin-bottom: 20px;
+
+  @media all and (min-width: 768px) {
+    flex: 1 0 auto;
+    margin-bottom: 0;
+  }
+`
+
 const Lasers: React.FC<Props> = ({ onClick }: Props) => {
   return (
     <Section>
@@ -23,7 +34,7 @@ const Lasers: React.FC<Props> = ({ onClick }: Props) => {
         <Card>
           <Row>
             {Array.from(LASERS).map(([laser, meta]) => (
-              <Col key={meta.name}>
+              <Col as={StyledCol} key={meta.name}>
                 <Variant title={meta.name} image={meta.src} onClick={() => onClick(laser)} />
               </Col>
             ))}
